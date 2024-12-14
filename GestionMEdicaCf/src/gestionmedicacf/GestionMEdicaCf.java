3ad627f96aece9a4d81c94f9553859becf761163
Dell 5421 <Dell 5421@Funes>
2024-12-14 16:56:40 -0600

    ExamenParcial3

---
+++ GestionMEdicaCf/src/gestionmedicacf/GestionMEdicaCf.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gestionmedicacf;
+
+/**
+ *
+ * @author Dell 5421
+ */
+public class GestionMEdicaCf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
